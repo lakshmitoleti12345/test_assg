b1ca85a0a11506be400b2c69c206623ea56eb24a
lakshmitoleti12345 <lakshmitoleti12345@gmail.com>
2026-04-22 16:52:29 +0530

    added for and while loop programs

---
+++ loops.py
@@ -0,0 +1,23 @@
+numbers = list(range(1, 11))
+
+for num in numbers:
+    print(num)
+print('-------------------------------------')
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+print("-------------------------")
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+print("-----------------------")
+numbers = [1, 2, 3, 4, 5]
+total = 0
+
+for num in numbers:
+    total += num
+
+print(total)
